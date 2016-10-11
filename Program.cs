4bea77e1caacce7b9ff1c15d535173938837e0e2
AlexMcArdle <bluejayskater@gmail.com>
2016-10-10 23:25:46 -0500

    add config file
    no more hardcoding!

--- Program.cs
+++ Program.cs
@@ -13,6 +13,8 @@ using Discord.API;
 using Newtonsoft.Json;
 using System.Data;
 using System.Reflection;
+using System.ComponentModel;
+using System.Configuration;
 
 namespace DiscordLCD
 {
@@ -22,75 +24,71 @@ namespace DiscordLCD
         {
             AsyncContext.Run(() => MainAsync(args));
         }
-
-        static async void MainAsync(string[] args)
+        
+        public static async void MainAsync(string[] args)
         {
-            bool done = false;
-            var connectedUsers = new Dictionary<ulong, string>();
+            // Initialize the LCD app
+            LogitechGSDK.LogiLcdInit("DiscordLCD", LogitechGSDK.LOGI_LCD_TYPE_MONO | LogitechGSDK.LOGI_LCD_TYPE_COLOR);
+
+            // Might need this stuff later
             var speakers = new List<ulong>();
-            ulong currentChannel;
-            ulong previousChannel;
-            string line0 = "";
-            string line1 = "";
-            string line2 = "";
-            string line3 = "";
-            LogitechGSDK.LogiLcdInit("TEST", LogitechGSDK.LOGI_LCD_TYPE_MONO | LogitechGSDK.LOGI_LCD_TYPE_COLOR);
-
-
-            string clientId = "229746606205829120";
+            Dictionary<ulong, string> connectedUsers = new Dictionary<ulong, string>();
             string[] scopes = new string[] { "rpc", "rpc.api" };
             string rpcToken = "";
 
-            DiscordRpcClient client = new DiscordRpcClient(clientId, "http://127.0.0.1");
-            //DiscordRpcConfig config = new DiscordRpcConfig();
+            // These variables are set using the App.config file
+            ulong channelID = DiscordLCD.Properties.Settings.Default.channelID;
+            string clientID = DiscordLCD.Properties.Settings.Default.clientID;
+            string clientSecret = DiscordLCD.Properties.Settings.Default.clientSecret;
+            string bearerToken = DiscordLCD.Properties.Settings.Default.bearerToken;
 
-
+            DiscordRpcClient client = new DiscordRpcClient(clientID, "http://127.0.0.1");
             RequestOptions requestOptions = new RequestOptions();
 
-            // Comment out if hardcoding bearertoken
-            string token = await client.AuthorizeAsync(scopes, rpcToken);
-
-            Discord.Net.Rest.DefaultRestClient restClient = new Discord.Net.Rest.DefaultRestClient("https://discordapp.com/api/");
-
-            IReadOnlyDictionary<string, object> request = new Dictionary<string, object>
+            if (bearerToken == "")
             {
-                { "client_id", clientId },
-                { "client_secret", "AKt3xJ2-RCQ2Msgxicte1ER1QFXR-RT9" },
-                { "grant_type", "authorization_code" },
-                { "code", token },
-                { "redirect_uri", "http://127.0.0.1" }
+                // get auth code from Discord client
+                string authcode = await client.AuthorizeAsync(scopes, rpcToken);
 
-            };
+                // get token using authcode
+                Discord.Net.Rest.DefaultRestClient restClient = new Discord.Net.Rest.DefaultRestClient("https://discordapp.com/api/");
+                IReadOnlyDictionary<string, object> request = new Dictionary<string, object>
+                {
+                    { "client_id", clientID },
+                    { "client_secret", clientSecret },
+                    { "grant_type", "authorization_code" },
+                    { "code", authcode },
+                    { "redirect_uri", "http://127.0.0.1" }
+                };
+                var stream = await restClient.SendAsync("POST", "oauth2/token", request, requestOptions);
+                stream.Position = 0;
+                var sr = new StreamReader(stream);
+                var json = sr.ReadToEnd();
+                Dictionary<string, string> response = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
 
-            var stream = await restClient.SendAsync("POST", "oauth2/token", request, requestOptions);
-            stream.Position = 0;
-            var sr = new StreamReader(stream);
-            var json = sr.ReadToEnd();
+                bearerToken = response["access_token"];
+                DiscordLCD.Properties.Settings.Default.bearerToken = bearerToken;
+                DiscordLCD.Properties.Settings.Default.Save();
+            }
 
-            Dictionary<string, string> response = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
-            string bearerToken = response["access_token"];
-            
-
-            //string bearerToken = "QfwHsIT5g4HPdVTxOD7x3LG9bdYFDa";
-
+            // Login and connect!
             await client.LoginAsync(TokenType.Bearer, bearerToken, false);
             await client.ConnectAsync();
 
-            ulong channelId = 229756130098806785;
+            await client.SubscribeChannel(channelID, RpcChannelEvent.SpeakingStart);
+            await client.SubscribeChannel(channelID, RpcChannelEvent.SpeakingStop);
+            await client.SubscribeChannel(channelID, RpcChannelEvent.VoiceStateCreate);
+            await client.SubscribeChannel(channelID, RpcChannelEvent.VoiceStateDelete);
+            await client.SubscribeChannel(channelID, RpcChannelEvent.VoiceStateUpdate);
             
-            await client.SubscribeChannel(channelId, RpcChannelEvent.SpeakingStart);
-            await client.SubscribeChannel(channelId, RpcChannelEvent.SpeakingStop);
-            await client.SubscribeChannel(channelId, RpcChannelEvent.VoiceStateCreate);
-            await client.SubscribeChannel(channelId, RpcChannelEvent.VoiceStateDelete);
-            await client.SubscribeChannel(channelId, RpcChannelEvent.VoiceStateUpdate);
-
+// i don't like the color of await warnings
 #pragma warning disable 1998
             client.ApiClient.ReceivedRpcEvent += async (s1,s2,o1) => {
                 Console.WriteLine("ReceivedRpcEvent");
                 Console.WriteLine("s1: " + s1);
                 Console.WriteLine("s2: " + s2);
                 Console.WriteLine("o1: " + o1);
-                line2 = s2.ToString();
+                //line2 = s2.ToString();
 
                 if (s2.ToString() == "SPEAKING_START")
                 {
@@ -120,13 +118,13 @@ namespace DiscordLCD
                     string value;
                     if (!connectedUsers.TryGetValue((ulong)jObject["user"]["id"], out value))
                     {
-                        connectedUsers.Add((ulong)jObject["user"]["id"], (string)jObject["user"]["username"]);
-                        Console.WriteLine("User added");
+                        var name = jObject["nick"].ToString() ?? jObject["user"]["username"].ToString();
+                        connectedUsers.Add((ulong)jObject["user"]["id"], name);
+                        Console.WriteLine("User added to connectedUsers. Total: " + connectedUsers.Count);
                     }
                 }
+                UpdateLCD(client, speakers, connectedUsers);
             };
-#pragma warning restore 1998
-
             client.Log += async (logMessage) =>
             {
                 Console.WriteLine("Log Event");
@@ -156,53 +154,50 @@ namespace DiscordLCD
             {
                 Console.WriteLine("VoiceStateDeleted: " + state);
             };
-            //client.MessageReceived += async (s, e) => {
-            //    Console.WriteLine(e.Channel.ToString());
-            //};
+            client.MessageReceived += async (m) => {
+                Console.WriteLine("MessageReceived: " + m.ToString());
+            };
+#pragma warning restore 1998
+
 
 
 
             Console.WriteLine("Current Status: " + client.ConnectionState);
-            line1 = "0";
 
-            while (!done)
+            // Wait for keypress before closing
+            Console.Read();
+        }
+
+        private static void UpdateLCD(DiscordRpcClient client, List<ulong> speakers, Dictionary<ulong, string> connectedUsers)
+        {
+            Console.WriteLine("Length: " + speakers.Count);
+
+            string line0 = "";
+            string line1 = "";
+            string line2 = "";
+            string line3 = "";
+
+            // put all the speakers in a string. create a copy of the list first
+            string speakersString = "";
+            foreach (ulong s in speakers.ToList())
             {
-                LogitechGSDK.LogiLcdUpdate();
-
-                //string text = Console.ReadLine();
-                //if (text.Equals("#!done"))
-                //{
-                //    LogitechGSDK.LogiLcdShutdown();
-                //    done = true;
-                //}
-                //line0 = line1;
-                //line1 = line2;
-                line3 = client.ConnectionState.ToString();
-
-                // just a counter
-                //int l1 = Int32.Parse(line1);
-                //l1++;
-                //line1 = l1.ToString();
-
-                // put all the speakers in a string. create a copy of the list first
-                string speakersString = " ";
-                foreach (ulong s in speakers.ToList())
+                string user;
+                if (connectedUsers.TryGetValue(s, out user))
                 {
-                    string user;
-                    if (connectedUsers.TryGetValue(s, out user))
-                    {
-                        speakersString += user + " ";
-                    }
+                    speakersString += user + " ";
                 }
-
-                line2 = speakersString;
-
-                LogitechGSDK.LogiLcdMonoSetText(0, line0);
-                LogitechGSDK.LogiLcdMonoSetText(1, line1);
-                LogitechGSDK.LogiLcdMonoSetText(2, line2);
-                LogitechGSDK.LogiLcdMonoSetText(3, line3);
             }
-            LogitechGSDK.LogiLcdShutdown();
+
+            line2 = speakersString;
+            line3 = client.ConnectionState.ToString();
+
+            Console.WriteLine("speakerstring: " + speakersString);
+
+            LogitechGSDK.LogiLcdMonoSetText(0, line0);
+            LogitechGSDK.LogiLcdMonoSetText(1, line1);
+            LogitechGSDK.LogiLcdMonoSetText(2, line2);
+            LogitechGSDK.LogiLcdMonoSetText(3, line3);
+            LogitechGSDK.LogiLcdUpdate();
         }
     }
 }
